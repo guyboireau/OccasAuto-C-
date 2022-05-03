d755bab94b85132909f956d414a8c80aff1dfd32
guyboireau <81310568+guyboireau@users.noreply.github.com>
2022-05-03 09:32:50 +0200

    BD

---
+++ ScriptOcassAuto.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS bdOccasion;
+use bdOccasion;
+
+DROP TABLE IF EXISTS Concessionaire;
+
+CREATE TABLE IF NOT EXISTS Concessionaire
+(
+	idConces INT(2) auto_increment,
+    nom VARCHAR(15),
+    prenom VARCHAR(15),
+    adresse VARCHAR(50),
+    codePostal VARCHAR(5),
+    ville VARCHAR(20),
+    PRIMARY KEY (idConces) 
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO Concessionaire (nom,prenom,adresse,codePostal,ville) VALUES 
+('Davolio', 'Jeanne', '5 rue de la poste', '63000', 'CLERMONT-FERRAND'),
+('Fuller', 'Philippe', '254 avenue Berthelot', '63000', 'CLERMONT-FERRAND'),
+('Daverling', 'Laura', '1 place de Jaude', '63000', 'CLERMONT-FERRAND'),
+('Peacock', 'Emilie', '23, Impasse des Fleurs', '63000', 'CLERMONT-FERRAND'),
+('Buchanan', 'Pierre', '32 place de l''Hôtel de Ville', '63200', 'RIOM'),
+('Bucyama', 'Michel', '15 rue des pinsons', '63200', 'RIOM'),
+('King', 'Louis', '57 rue de la gare', '63500', 'ISSOIRE'),
+('Callahan', 'Jean', '23 boulevard Central', '63500', 'ISSOIRE'),
+('Bucsworth', 'Pierre', '7 rue de la paix', '63500', 'ISSOIRE');
