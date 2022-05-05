5b5efacd111b507976aeb1e9b91aaf4d651b1699
guyboireau <81310568+guyboireau@users.noreply.github.com>
2022-05-05 12:21:10 +0200

    Add files via upload

---
+++ AP -OCCAS AUTO- GUY BOIREAU/BiblioConces/AutoConces/Connexion.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace AutoConces
+{
+    partial class Connexion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connexionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inscriptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(101, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Login : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mot de passe : ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Se connecter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "S\'inscrire";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Mot de passe : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Login : ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(110, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(110, 72);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Mail  : ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(110, 99);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connexionToolStripMenuItem,
+            this.inscriptionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // connexionToolStripMenuItem
+            // 
+            this.connexionToolStripMenuItem.Name = "connexionToolStripMenuItem";
+            this.connexionToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.connexionToolStripMenuItem.Text = "Connexion";
+            this.connexionToolStripMenuItem.Click += new System.EventHandler(this.connexionToolStripMenuItem_Click);
+            // 
+            // inscriptionToolStripMenuItem
+            // 
+            this.inscriptionToolStripMenuItem.Name = "inscriptionToolStripMenuItem";
+            this.inscriptionToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.inscriptionToolStripMenuItem.Text = "Inscription";
+            this.inscriptionToolStripMenuItem.Click += new System.EventHandler(this.inscriptionToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(247, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(225, 239);
+            this.panel1.TabIndex = 13;
+            this.panel1.Visible = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(12, 41);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(220, 239);
+            this.panel2.TabIndex = 14;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.textBox6);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Location = new System.Drawing.Point(478, 41);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(236, 239);
+            this.panel3.TabIndex = 15;
+            this.panel3.Visible = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(61, 38);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Mail : ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(47, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Envoyer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(58, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 13);
+            this.label7.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(58, 163);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 15);
+            this.label8.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(56, 208);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 21);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Mot de passe oublié";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Connexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Connexion";
+            this.Text = "Connexion";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem connexionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inscriptionToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ AP -OCCAS AUTO- GUY BOIREAU/BiblioConces/AutoConces/Connexion.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using BiblioConces;
+using System.Text.RegularExpressions;
+
+namespace AutoConces
+{
+    public partial class Connexion : Form
+    {
+        public Connexion()
+        {
+            InitializeComponent();
+        }
+        public static string RemoveSpecialCharacters(string input)
+        {
+            Regex r = new Regex("(?:[^a-z0-9 ]|(?<=['\"])s)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+            return r.Replace(input, String.Empty);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string login = RemoveSpecialCharacters(textBox1.Text);
+            string mdp = RemoveSpecialCharacters(textBox2.Text);
+
+            if (login != String.Empty && mdp != String.Empty)
+            {              
+                   
+
+                //string regex = "^.*$";
+                /*string mdp = Regex.Matches(textBox2.Text.ToString(),regex ).ToString();
+                string login = Regex.Matches(textBox1.Text.ToString(), regex).ToString();*/
+
+                String chConnexion = "SERVER=localhost;DATABASE=bdoccasion2; UID=root;";
+                MySqlConnection cnx = new MySqlConnection(chConnexion);
+                cnx.Open();
+                //choix de la recherche
+                string req = "select pseudo,mdp from Users where pseudo ='" + login + "'";
+                MySqlCommand cmd = new MySqlCommand(req, cnx);
+                MySqlDataReader lecteur = cmd.ExecuteReader();
+                while (lecteur.Read())
+                {
+                    if (lecteur[0] == null || lecteur[1] == null)
+                    {
+                        lecteur.Close();
+                        MessageBox.Show("Impossible de vous connecter");
+                    }
+                    else if (lecteur[0].ToString().Equals(login) && lecteur[1].ToString().Equals(mdp))
+                    {
+                        Close();
+                    }
+                    else if (!lecteur[1].ToString().Equals(mdp))
+                    {
+                        MessageBox.Show("Impossible de vous connecter, réessayer");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vous n'avez pas rempli tous les champs");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string login = RemoveSpecialCharacters(textBox3.Text);
+            string mail = RemoveSpecialCharacters(textBox4.Text);
+            string mdp = RemoveSpecialCharacters(textBox5.Text);
+            if (mdp != String.Empty && mail != String.Empty && login != String.Empty)
+            {
+                String chConnexion = "SERVER=localhost;DATABASE=bdoccasion2; UID=root;";
+                MySqlConnection cnx = new MySqlConnection(chConnexion);
+                cnx.Open();
+                //choix de la recherche
+                string req = "insert into users (pseudo, mdp, mail) values (@login,@mail,@mdp)";
+                MySqlCommand cmdAjout = new MySqlCommand(req, cnx);
+                cmdAjout.Parameters.Add("@login", MySqlDbType.VarChar);
+                cmdAjout.Parameters["@login"].Value = login;
+
+                cmdAjout.Parameters.Add("@mail", MySqlDbType.VarChar);
+                cmdAjout.Parameters["@mail"].Value = mail;
+
+                cmdAjout.Parameters.Add("@mdp", MySqlDbType.VarChar);
+                cmdAjout.Parameters["@mdp"].Value = mdp;
+
+                try//Exeception pour erreur de saisi dans insertion
+                {
+                    cmdAjout.ExecuteNonQuery();
+                }
+                catch
+                {
+                    MessageBox.Show("Vous n'avez pas saisi correctement.");
+                }
+
+                panel1.Visible = false;
+                panel2.Visible = true;
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void connexionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = true;
+            panel3.Visible = false;
+        }
+
+        private void inscriptionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            panel2.Visible = false;
+            panel3.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*string mail = RemoveSpecialCharacters(textBox6.Text);*/
+            string mail = textBox6.Text;
+
+            if (mail != String.Empty)
+            {
+
+
+                //string regex = "^.*$";
+                /*string mdp = Regex.Matches(textBox2.Text.ToString(),regex ).ToString();
+                string login = Regex.Matches(textBox1.Text.ToString(), regex).ToString();*/
+
+                String chConnexion = "SERVER=localhost;DATABASE=bdoccasion2; UID=root;";
+                MySqlConnection cnx = new MySqlConnection(chConnexion);
+                cnx.Open();
+                //choix de la recherche
+                string req = "select pseudo,mdp,mail from Users where mail ='" + mail + "'";
+                MySqlCommand cmd = new MySqlCommand(req, cnx);
+                MySqlDataReader lecteur = cmd.ExecuteReader();
+                while (lecteur.Read())
+                {
+                    if (lecteur[0] == null || lecteur[1] == null)
+                    {
+                        lecteur.Close();
+                        MessageBox.Show("Impossible de vous connecter");
+                    }               
+                    else if (lecteur[2].ToString().Equals(mail))
+                    {
+                        label7.Text = "Login : "+ lecteur[0].ToString();
+                        label8.Text = "Mdp : " + lecteur[1].ToString();
+                    }
+                    else if (!lecteur[2].ToString().Equals(mail))
+                    {
+                        MessageBox.Show("Impossible de vous connecter, réessayer");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vous n'avez pas rempli tous les champs");
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel2.Visible = false;
+            panel3.Visible = true;
+        }
+    }
+}
